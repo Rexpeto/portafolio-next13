7433d272f2d22af57e45736fd942d3bef22a90ab
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-01 18:32:26 -0400

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
   };
 
   return (
-    <div className="bg-primary/60  flex flex-col xl:flex-row justify-between items-center h-full xl:h-screen">
-      <div className="xl:container xl:mx-auto pt-16">
+    <div className="bg-primary/60  flex flex-col xl:flex-row xl:justify-between justify-center items-center h-full xl:h-screen">
+      <div className="xl:container xl:mx-auto">
         <div className="w-full h-full mt-8 xl:mt-0">
           <motion.h1
             className="h1"
@@ -61,7 +61,7 @@ export default function Home() {
         initial="hidden"
         animate="show"
         exit="hidden"
-        className="xl:w-[800px] w-full xl:h-full absolute right-0 bottom-0 z-20"
+        className="xl:w-[800px] w-full xl:h-full absolute right-0 bottom-0 z-20 hidden xl:block"
       >
         <Image
           src="/avatar.png"
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,17 @@ import GetIcon from "./GetIcon";
 const Header = () => {
   return (
     <header className="absolute flex items-center px-16 xl:px-0 xl:h-[90px] w-full z-30 pb-4">
-      <div className="xl:container xl:mx-auto py-8 w-full flex items-center justify-between">
+      <div className="xl:container xl:mx-auto py-8 w-full flex flex-col xl:flex-row items-center justify-between gap-4">
         <Link href="/">
           <Image src="/logo.svg" alt="Carlos Gallardo" width={50} height={50} />
         </Link>
-        <div className="flex items-center gap-6 xl:gap-4">
+        <div className="flex items-center gap-8 xl:gap-4">
           {socialData.map((social) => (
             <Link href={social.path} key={social.name} target="_blank">
-              <GetIcon icon={social.icon} className="text-xl xl:text-lg" />
+              <GetIcon
+                icon={social.icon}
+                className="text-2xl xl:text-lg hover:text-accent transition duraction-150"
+              />
             </Link>
           ))}
         </div>
